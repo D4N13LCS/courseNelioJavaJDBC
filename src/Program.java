c10d861b642f28768a482dae0a3c2aee3354cd1c
D4N13LCS <danielmazzini6@gmail.com>
2026-04-01 21:47:50 -0300

    Creationg Project and git repository

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+
+import db.Db;
+import java.sql.Connection;
+
+public class Program{
+    public static void main(String[] args){
+        Connection conn = Db.getConnection();
+        Db.closeConnection();
+    }
+}
